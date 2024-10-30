0f715ed70f38fd9b162be4dde5d7e5d27e5c88a4
nloukei <2201103742@student.buksu.edu.ph>
2024-10-30 16:39:15 +0800

    Add user component

--- client/src/pages/userpages/home.jsx
+++ client/src/pages/userpages/home.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Footer from '../components/footer/footer.jsx';
-import NavbarWithSidebar from '../components/sidebar/sidebar.jsx';
-import Home from '../components/homecontent/home-content.jsx'
+import Footer from '../../components/footer/footer.jsx';
+import NavbarWithSidebar from '../../components/sidebar/sidebar.jsx';
+import Home from '../../components/homecontent/home-content.jsx'
 
-import Events from '../components/events-grid/events-grid.jsx';
+import Events from '../../components/events-grid/events-grid.jsx';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
