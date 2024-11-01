b7ca9ad2698b26557bc270539170124de2a3b8d5
Fibii44 <2201102089@student.buksu.edu.ph>
2024-11-01 10:59:57 +0800

    Added new Calendar UI for the admin

--- client/src/pages/admin/calendar.jsx
+++ client/src/pages/admin/calendar.jsx
@@ -1,25 +1,54 @@
-import React, { useState } from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import React, { useState } from 'react';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import Calendar  from '../../components/admin/calendar/calendar';
+// import Calendar  from '../../components/admin/calendar/calendar';
+// import Sidebar from '../../components/admin/adminbar/sidebar';
+// import Topbar from '../../components/admin/adminbar/topbar';
+
+// const adminCalendar =  () => {
+//     const [isCollapsed, setIsCollapsed] = useState(false);
+//     const toggleSidebar = () => {
+//         setIsCollapsed(!isCollapsed);
+//     }
+//     return(
+//         <div className="dashboard-container">
+//             <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} activePage="calendar" />
+//             <div className="content">
+//                 <Topbar />
+//                 <div className="context-card">
+//                     <Calendar />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// };
+// export default adminCalendar;
+
+import React, { useState } from 'react';
 import Sidebar from '../../components/admin/adminbar/sidebar';
 import Topbar from '../../components/admin/adminbar/topbar';
+import Calendar from '../../components/admin/calendar/calendar'
 
-const adminCalendar =  () => {
+
+const CalendarAdmin = () => {
     const [isCollapsed, setIsCollapsed] = useState(false);
+
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
-    }
-    return(
+    };
+
+    return (
         <div className="dashboard-container">
             <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} activePage="calendar" />
             <div className="content">
                 <Topbar />
-                <div className="context-card">
-                    <Calendar />
+                <div className="calendar-section">
+                    <Calendar /> {/* Calendar component integration */}
                 </div>
             </div>
         </div>
-    )
+    );
 };
-export default adminCalendar;
+
+export default CalendarAdmin;
+
