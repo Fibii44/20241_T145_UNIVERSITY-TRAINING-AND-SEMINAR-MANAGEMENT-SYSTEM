bbf581335d1a3b808d06529c1075092312bf85aa
nloukei <2201103742@student.buksu.edu.ph>
2024-11-07 19:19:11 +0800

    Impoved UI for homepage

--- client/src/components/admin/adminbar/sidebar.jsx
+++ client/src/components/admin/adminbar/sidebar.jsx
@@ -1,77 +1,97 @@
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTachometerAlt, faCalendarCheck, faClock, faCog, faSignOutAlt, faBars, faUsers, faCalendar, faHistory  } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom'; // Import Link
+import { faTachometerAlt, faCalendarCheck, faClock, faCog, faSignOutAlt, faBars, faUsers, faCalendar, faHistory } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom'; 
 import './css/sidebar.css';
 
+const Sidebar = ({ activePage }) => {
+  // Retrieve the initial state from localStorage or set default to true (collapsed)
+  const [isCollapsed, setIsCollapsed] = useState(() => {
+    return localStorage.getItem('isSidebarCollapsed') === 'true';
+  });
 
-const Sidebar = ({ isCollapsed, toggleSidebar, activePage }) => (
-  <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-    <div className="sidebar-header">
-      <button className="menu-toggle" onClick={toggleSidebar}>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-      <div className="sidebar-title">
-        <h2 className="buksu">{!isCollapsed && 'BukSU'}</h2>
-        <h2 className="engage">{!isCollapsed && 'Engage'}</h2>
+  const toggleSidebar = () => {
+    // Toggle the state and store the updated state in localStorage
+    setIsCollapsed((prevState) => {
+      const newState = !prevState;
+      localStorage.setItem('isSidebarCollapsed', newState);
+      return newState;
+    });
+  };
+
+  useEffect(() => {
+    // Update the collapsed state when the component mounts
+    const savedState = localStorage.getItem('isSidebarCollapsed') === 'true';
+    setIsCollapsed(savedState);
+  }, []);
+
+  return (
+    <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+      <div className="sidebar-header">
+        <button className="menu-toggle" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div className="sidebar-title">
+          <h2 className="buksu">{!isCollapsed && 'BukSU'}</h2>
+          <h2 className="engage">{!isCollapsed && 'Engage'}</h2>
+        </div>
       </div>
       
-      
-    </div>
-    
-    <ul className="menu-items">
-      <li className={activePage === 'dashboard' ? 'active' : ''}>
-        <Link to="/a/dashboard"> {/* Add link to Dashboard */}
-          <span className="icon"><FontAwesomeIcon icon={faTachometerAlt} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Dashboard</span>}
-        </Link>
-      </li>
-      <li className={activePage === 'calendar' ? 'active' : ''}>
-        <Link to="/a/calendar"> {/* Add link to Calendar */}
-          <span className="icon"><FontAwesomeIcon icon={faCalendarCheck} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Calendar</span>}
-        </Link>
-      </li>
-      <li className={activePage === 'personnel' ? 'active' : ''}>
-        <Link to="/a/personnel"> {/* Add link to Personnel */}
-          <span className="icon"><FontAwesomeIcon icon={faClock} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Personnel</span>}
-        </Link>
-      </li>
-      <li className={activePage === 'users' ? 'active' : ''}>
-        <Link to="/a/users"> {/* Add link to Users Table */}
-          <span className="icon"><FontAwesomeIcon icon={faUsers} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Users</span>}
-        </Link>
-      </li>
-      <li className={activePage === 'history' ? 'active' : ''}>
-        <Link to="/a/history"> {/* Add link to Users Table */}
-          <span className="icon"><FontAwesomeIcon icon={faHistory} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>History</span>}
-        </Link>
-      </li>
-      <li className={activePage === 'events' ? 'active' : ''}>
-        <Link to="/a/events"> {/* Add link to Events */}
-          <span className="icon"><FontAwesomeIcon icon={faCalendar} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Events</span>}
-        </Link>
-      </li>
-    </ul>
+      <ul className="menu-items">
+        <li className={activePage === 'dashboard' ? 'active' : ''}>
+          <Link to="/a/dashboard">
+            <span className="icon"><FontAwesomeIcon icon={faTachometerAlt} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Dashboard</span>}
+          </Link>
+        </li>
+        <li className={activePage === 'calendar' ? 'active' : ''}>
+          <Link to="/a/calendar">
+            <span className="icon"><FontAwesomeIcon icon={faCalendarCheck} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Calendar</span>}
+          </Link>
+        </li>
+        <li className={activePage === 'personnel' ? 'active' : ''}>
+          <Link to="/a/personnel">
+            <span className="icon"><FontAwesomeIcon icon={faClock} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Personnel</span>}
+          </Link>
+        </li>
+        <li className={activePage === 'users' ? 'active' : ''}>
+          <Link to="/a/users">
+            <span className="icon"><FontAwesomeIcon icon={faUsers} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Users</span>}
+          </Link>
+        </li>
+        <li className={activePage === 'history' ? 'active' : ''}>
+          <Link to="/a/history">
+            <span className="icon"><FontAwesomeIcon icon={faHistory} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>History</span>}
+          </Link>
+        </li>
+        <li className={activePage === 'events' ? 'active' : ''}>
+          <Link to="/a/events">
+            <span className="icon"><FontAwesomeIcon icon={faCalendar} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Events</span>}
+          </Link>
+        </li>
+      </ul>
 
-    <ul className="sidebar-footer"> 
-      <li className={`sidebar-footer-item ${activePage === 'settings' ? 'active' : ''}`}>
-        <Link to="/settings"> {/* Add link to Settings */}
-          <span className="icon"><FontAwesomeIcon icon={faCog} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Settings</span>}
-        </Link>
-      </li>
-      <li className={`sidebar-footer-item ${activePage === 'logout' ? 'active' : ''}`}>
-        <Link to="/logout"> {/* Add link to Logout */}
-          <span className="icon"><FontAwesomeIcon icon={faSignOutAlt} size="lg" /></span>
-          {!isCollapsed && <span className='list-name'>Logout</span>}
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+      <ul className="sidebar-footer"> 
+        <li className={`sidebar-footer-item ${activePage === 'settings' ? 'active' : ''}`}>
+          <Link to="/settings">
+            <span className="icon"><FontAwesomeIcon icon={faCog} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Settings</span>}
+          </Link>
+        </li>
+        <li className={`sidebar-footer-item ${activePage === 'logout' ? 'active' : ''}`}>
+          <Link to="/logout">
+            <span className="icon"><FontAwesomeIcon icon={faSignOutAlt} size="lg" /></span>
+            {!isCollapsed && <span className='list-name'>Logout</span>}
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Sidebar;
--- client/src/components/user/sidebar/sidebar.jsx
+++ client/src/components/user/sidebar/sidebar.jsx
@@ -1,84 +1,98 @@
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCertificate, faHistory, faCalendar, faCog, faSignOutAlt, faBars, faBell, faHome, faCalendarCheck, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faCertificate, faHistory, faCalendar, faCog, faSignOutAlt, faBars, faHome, faCalendarCheck, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import '../../../components/admin/adminbar/css/admin.css';
 
-const Sidebar = ({ isCollapsed, toggleSidebar, activePage }) => (
-  <div className={`sidebar ${isCollapsed ? 'collapsed' : ''} d-none d-md-flex`}>
-    <div className="sidebar-header">
-      <button className="menu-toggle" onClick={toggleSidebar}>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
+const Sidebar = ({ activePage }) => {
+  // Retrieve the initial state from localStorage or set default to true (collapsed)
+  const [isCollapsed, setIsCollapsed] = useState(() => {
+    return localStorage.getItem('isSidebarCollapsed') === 'true';
+  });
 
-      <div className="sidebar-title">
-        <h2 className="buksu">{!isCollapsed && 'BukSU'}</h2>
-        <h2 className="engage">{!isCollapsed && 'Engage'}</h2>
-      </div>
-      
-    </div>
-    <ul className="menu-items">
+  const toggleSidebar = () => {
+    // Toggle the state and store the updated state in localStorage
+    setIsCollapsed((prevState) => {
+      const newState = !prevState;
+      localStorage.setItem('isSidebarCollapsed', newState);
+      return newState;
+    });
+  };
 
-{/* ============================================================================================ */}
-      <li className={activePage === 'home' ? 'active' : ''}>
-        <Link to="/u/home">
-          <span className="icon"><FontAwesomeIcon icon={faHome} size="lg" /></span>
-          {!isCollapsed && <span>Home</span>}
-        </Link>
-      </li>
+  useEffect(() => {
+    // Update the collapsed state when the component mounts
+    const savedState = localStorage.getItem('isSidebarCollapsed') === 'true';
+    setIsCollapsed(savedState);
+  }, []);
 
-{/* ============================================================================================ */}
-      <li className={activePage === 'events' ? 'active' : ''}>
-        <Link to="/u/events">
-          <span className="icon"><FontAwesomeIcon icon={faCalendarCheck} size="lg" /></span>
-          {!isCollapsed && <span>Events</span>}
-        </Link>
-      </li>
-{/* ============================================================================================ */}
-      <li className={activePage === 'calendar' ? 'active' : ''}>
-        <Link to="/u/calendar">
-          <span className="icon"><FontAwesomeIcon icon={faCalendar} size="lg" /></span>
-          {!isCollapsed && <span>Calendar</span>}
-        </Link>
-      </li>
-{/* ============================================================================================ */}
-      <li className={activePage === 'certificates' ? 'active' : ''}>
-        <Link to="/u/certificates">
-          <span className="icon"><FontAwesomeIcon icon={faCertificate} size="lg" /></span>
-          {!isCollapsed && <span>Certificates</span>}
-        </Link>
-      </li>
-{/* ============================================================================================ */}
-      <li className={activePage === 'history' ? 'active' : ''}>
-        <Link to="/u/history">
-          <span className="icon"><FontAwesomeIcon icon={faHistory} size="lg" /></span>
-          {!isCollapsed && <span>History</span>}
-        </Link>
-      </li>
-{/* ============================================================================================ */}
-      <li className={activePage === 'profile' ? 'active' : ''}>
-        <Link to="/u/profile">
-          <span className="icon"><FontAwesomeIcon icon={faUser} size="lg" /></span>
-          {!isCollapsed && <span>Profile</span>}
-        </Link>
-      </li>       
-    </ul>
+    return (
+        <div className={`sidebar ${isCollapsed ? 'collapsed' : ''} d-none d-md-flex`}>
+            <div className="sidebar-header">
+                <button className="menu-toggle" onClick={toggleSidebar}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
 
-    
-    <ul className="sidebar-footer">
-      <li className={`sidebar-footer-item ${activePage === 'settings' ? 'active' : ''}`}>
-        <Link to="/settings">
-          <span className="icon"><FontAwesomeIcon icon={faCog} size="lg" /></span>
-          {!isCollapsed && <span>Settings</span>}
-        </Link>
-      </li>
-      <li className={`sidebar-footer-item ${activePage === 'logout' ? 'active' : ''}`}>
-        <Link to="/logout">
-          <span className="icon"><FontAwesomeIcon icon={faSignOutAlt} size="lg" /></span>
-          {!isCollapsed && <span>Logout</span>}
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+                <div className="sidebar-title">
+                    <h2 className="buksu">{!isCollapsed && 'BukSU'}</h2>
+                    <h2 className="engage">{!isCollapsed && 'Engage'}</h2>
+                </div>
+            </div>
+            
+            <ul className="menu-items">
+                <li className={activePage === 'home' ? 'active' : ''}>
+                    <Link to="/u/home">
+                        <span className="icon"><FontAwesomeIcon icon={faHome} size="lg" /></span>
+                        {!isCollapsed && <span>Home</span>}
+                    </Link>
+                </li>
+                <li className={activePage === 'events' ? 'active' : ''}>
+                    <Link to="/u/events">
+                        <span className="icon"><FontAwesomeIcon icon={faCalendarCheck} size="lg" /></span>
+                        {!isCollapsed && <span>Events</span>}
+                    </Link>
+                </li>
+                <li className={activePage === 'calendar' ? 'active' : ''}>
+                    <Link to="/u/calendar">
+                        <span className="icon"><FontAwesomeIcon icon={faCalendar} size="lg" /></span>
+                        {!isCollapsed && <span>Calendar</span>}
+                    </Link>
+                </li>
+                <li className={activePage === 'certificates' ? 'active' : ''}>
+                    <Link to="/u/certificates">
+                        <span className="icon"><FontAwesomeIcon icon={faCertificate} size="lg" /></span>
+                        {!isCollapsed && <span>Certificates</span>}
+                    </Link>
+                </li>
+                <li className={activePage === 'history' ? 'active' : ''}>
+                    <Link to="/u/history">
+                        <span className="icon"><FontAwesomeIcon icon={faHistory} size="lg" /></span>
+                        {!isCollapsed && <span>History</span>}
+                    </Link>
+                </li>
+                <li className={activePage === 'profile' ? 'active' : ''}>
+                    <Link to="/u/profile">
+                        <span className="icon"><FontAwesomeIcon icon={faUser} size="lg" /></span>
+                        {!isCollapsed && <span>Profile</span>}
+                    </Link>
+                </li>
+            </ul>
+
+            <ul className="sidebar-footer">
+                <li className={`sidebar-footer-item ${activePage === 'settings' ? 'active' : ''}`}>
+                    <Link to="/settings">
+                        <span className="icon"><FontAwesomeIcon icon={faCog} size="lg" /></span>
+                        {!isCollapsed && <span>Settings</span>}
+                    </Link>
+                </li>
+                <li className={`sidebar-footer-item ${activePage === 'logout' ? 'active' : ''}`}>
+                    <Link to="/logout">
+                        <span className="icon"><FontAwesomeIcon icon={faSignOutAlt} size="lg" /></span>
+                        {!isCollapsed && <span>Logout</span>}
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 export default Sidebar;
