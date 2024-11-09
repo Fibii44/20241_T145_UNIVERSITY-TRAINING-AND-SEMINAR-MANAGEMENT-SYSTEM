4702e4f46fc4fb1398733b29e27f7c5eaf30ffbd
Bbyan09 <2201103762@student.buksu.edu.ph>
2024-11-09 23:19:49 +0800

    Updated Logout

--- client/src/components/user/eventDetails/eventDetails.jsx
+++ client/src/components/user/eventDetails/eventDetails.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '@fontsource/nunito-sans'; 
 import { useParams } from 'react-router-dom';
 import './eventDetails.css'
 
