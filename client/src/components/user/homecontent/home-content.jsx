fa4f94da54877b46f7ac5e2983caf9cd67a6f849
nloukei <2201103742@student.buksu.edu.ph>
2024-11-28 00:24:39 +0800

    Improvised Home Page

--- client/src/components/user/homecontent/home-content.jsx
+++ client/src/components/user/homecontent/home-content.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import './home-content.css';
+import bg1 from '../../../assets/homebg1.jpg';
+import bg2 from '../../../assets/homebg2.jpg';
+import bg3 from '../../../assets/homebg3.jpg';
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [backgroundIndex, setBackgroundIndex] = useState(0);
 
-  const handleSearch = () => {
-    // Logic to handle search
-    console.log(searchTerm);
-  };
+  // List of background images
+  const backgrounds = [
+    bg1,
+    bg2,
+    bg3,
+  ];
+
+  // Effect to change background every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBackgroundIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
+    }, 5000); // Change every 5000ms (5 seconds)
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [backgrounds.length]);
 
   return (
-    <div className="home-background">
+    <div
+      className="home-background"
+      style={{
+        backgroundImage: `url(${backgrounds[backgroundIndex]})`,
+      }}
+    >
       <div className="overlay"></div>
       <h3>
         <span>BukSU</span> <span className="highlight">Engage</span>
